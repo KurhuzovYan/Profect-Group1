38a64428f3de969004d24c8330ec3ddf5cd9d6dc
Yan Kurhuzov <yan.dasty@gmail.com>
2023-05-18 15:21:27 +0300

    Commit 6: testing

--- src/main/java/AppLauncher.java
+++ src/main/java/AppLauncher.java
@@ -2,5 +2,6 @@ public class AppLauncher {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("hello");
+        System.out.println("new sout");
     }
 }
