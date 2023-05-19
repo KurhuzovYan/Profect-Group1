ab634e2c109d3145b155817f5619484660375f81
Andrey-m01 <andrey-m1@bigmir.net>
2023-05-19 21:44:39 +0300

    First test

---
+++ src/main/java/servises/CurrencyInt.java
@@ -0,0 +1,9 @@
+package servises;
+
+import java.util.Date;
+
+public interface CurrencyInt {
+    Date lastUpdate = null;
+    Double usdCurr = 0d;
+
+}
---
+++ src/main/java/servises/ResponseMono.java
@@ -0,0 +1,13 @@
+package servises;
+
+import lombok.Data;
+
+
+public class ResponseMono {
+        public int currencyCodeA;
+        public int currencyCodeB;
+        public int date;
+        public double rateBuy;
+        public double rateCross;
+        public double rateSell;
+}
