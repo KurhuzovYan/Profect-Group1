dfbc9802501600383a2e3494338971d4ebc8175b
Nikola2112 <nikola21122004@gmail.com>
2023-05-19 11:37:57 +0100

    add privatApi

--- src/main/java/AppLauncher.java
+++ src/main/java/AppLauncher.java
@@ -1,6 +1,8 @@
 public class AppLauncher {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        PrivatBankAPIParser parser = new PrivatBankAPIParser();
+        String response = parser.sendRequest("p24api/pubinfo?exchange&json");
+        System.out.println(response);
     }
 
 }
