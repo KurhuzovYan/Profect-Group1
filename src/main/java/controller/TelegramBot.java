9541f6cd637c70d4344e8e82808f8ad7353da7ee
yan <yan.dasty@gmail.com>
2023-05-21 21:38:32 +0300

    Commit 14: Finished a method that returns buttons with a number after the dot

--- src/main/java/controller/TelegramBot.java
+++ src/main/java/controller/TelegramBot.java
@@ -6,26 +6,13 @@ import static util.ButtonCreater.*;
 import lombok.SneakyThrows;
 import org.telegram.telegrambots.extensions.bots.commandbot.TelegramLongPollingCommandBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
-import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageMedia;
+import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageReplyMarkup;
 import org.telegram.telegrambots.meta.api.objects.CallbackQuery;
-import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 import services.commands.StartCommand;
-import util.ButtonCreater;
-
-import java.awt.*;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.stream.Stream;
-
 
 public class TelegramBot extends TelegramLongPollingCommandBot {
-
-    private ButtonCreater buttonCreater;
 
     public TelegramBot() {
         register(new StartCommand());
@@ -37,8 +24,6 @@ public class TelegramBot extends TelegramLongPollingCommandBot {
         if (update.hasCallbackQuery()) {
             final CallbackQuery callbackQuery = update.getCallbackQuery();
             final String data = callbackQuery.getData();
-            final Message message = update.getMessage();
-
 
             switch (data) {
                 case "1":
@@ -49,27 +34,40 @@ public class TelegramBot extends TelegramLongPollingCommandBot {
                             .build());
                     break;
                 case "2":
-                    List<List<InlineKeyboardButton>> buttonsCase2 = new ArrayList<>();
-                    List<InlineKeyboardButton> buttons1 = new ArrayList<>();
-                    List<InlineKeyboardButton> buttons2 = new ArrayList<>();
-                    List<InlineKeyboardButton> buttons3 = new ArrayList<>();
-                    List<InlineKeyboardButton> buttons4 = new ArrayList<>();
-                    buttons1.add(getInlineKeyboardButton("Кількість знаків після коми", "3"));
-                    buttons2.add(getInlineKeyboardButton("Банк", "4"));
-                    buttons3.add(getInlineKeyboardButton("Валюти", "5"));
-                    buttons4.add(getInlineKeyboardButton("Час оповіщень", "6"));
-                    buttonsCase2.add(buttons1);
-                    buttonsCase2.add(buttons2);
-                    buttonsCase2.add(buttons3);
-                    buttonsCase2.add(buttons4);
-
                     execute(SendMessage.builder()
                             .text("Налаштування")
                             .chatId(update.getCallbackQuery().getMessage().getChatId().toString())
-                            .replyMarkup(InlineKeyboardMarkup.builder().keyboard(buttonsCase2).build())
+                            .replyMarkup(createSettingsButtons())
                             .build());
+                    break;
+                case "3":
+                    execute(SendMessage.builder()
+                            .text("Кількість знаків після коми")
+                            .chatId(update.getCallbackQuery().getMessage().getChatId().toString())
+                            .replyMarkup(createButtonWithDigitsAfterDot())
+                            .build());
+                    break;
             }
+
+            if (data.equals("TwoDigitsAfterDot") || data.equals("ThreeDigitsAfterDot") || data.equals("FourDigitsAfterDot")) {
+                InlineKeyboardMarkup markup = createButtonWithDigitsAfterDot();
+
+                markup.getKeyboard().forEach(buttons ->
+                        buttons.stream()
+                                .filter(button -> button.getCallbackData().equals(data))
+                                .forEach(button -> button.setText(button.getText() + " ✅")));
+
+
+                EditMessageReplyMarkup editMarkup = new EditMessageReplyMarkup();
+                editMarkup.setChatId(callbackQuery.getMessage().getChatId().toString());
+                editMarkup.setMessageId(callbackQuery.getMessage().getMessageId());
+                editMarkup.setReplyMarkup(markup);
+
+                execute(editMarkup);
+            }
+            System.out.println(data);
         }
+
     }
 
     @Override
